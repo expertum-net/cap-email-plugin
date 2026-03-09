ad361ad5a95b6a49f3b9b378e66d6516e3f4cc93
Matthis Vansteenhuyse <matthis.vansteenhuyse@expertum.net>
2026-03-09 14:59:09 +0100

    fix: replace import.meta.dirname with Node 20-compatible equivalent

--- test/unit/template-engine.test.ts
+++ test/unit/template-engine.test.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import cds from "@sap/cds";
 import { renderTemplate, resolveTemplatePath, loadTemplate } from "../../lib/template-engine.js";
 import { TEMPLATE_DIR, TEMPLATE_EXT } from "../../lib/constants.js";
@@ -44,7 +45,7 @@ describe("resolveTemplatePath", () => {
 
 describe("loadTemplate", () => {
   beforeAll(() => {
-    cds.root = path.resolve(import.meta.dirname, "../test-app");
+    cds.root = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../test-app");
   });
 
   it("loads existing template file contents", async () => {
