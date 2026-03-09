4f6e0d82e1af003d78ead91e4b98b521c65f9fab
Matthis Vansteenhuyse <matthis.vansteenhuyse@expertum.net>
2026-03-09 14:09:40 +0100

    feat: implement {{placeholder}} template rendering

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const ANNOTATION_PREFIX = "@email";
 export const TEMPLATE_DIR = "email-templates";
 export const TEMPLATE_EXT = ".html";
+export const PLACEHOLDER_PATTERN = /\{\{(\w+)\}\}/g;
