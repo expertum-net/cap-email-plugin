da968a4743af8fadcf7b02d3fa518643658e1c4b
Matthis Vansteenhuyse <matthis.vansteenhuyse@expertum.net>
2026-03-23 15:51:48 +0100

    chore: remove unused GRAPH_API_BASE constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,6 @@ export const TEMPLATE_DIR = "email-templates";
 export const TEMPLATE_EXT = ".html";
 export const PLACEHOLDER_PATTERN = /\{\{(\w+)\}\}/g;
 export const EMAIL_PATTERN = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const GRAPH_API_BASE = "https://graph.microsoft.com/v1.0";
 export const DEFAULT_RETRY_ATTEMPTS = 3;
 export const RETRYABLE_STATUS_CODES = [429, 503, 504];
 
